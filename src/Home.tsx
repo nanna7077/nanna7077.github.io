5d8365cc1e3f5b58fcafe7097fe0c14cb3f56668
Nannan <nanna7077@gmail.com>
2024-04-21 11:23:33 +0530

    Fix: Hireme Opacity

--- src/Home.tsx
+++ src/Home.tsx
@@ -34,35 +34,23 @@ export default function Home() {
             </div>
             <div className="mt-8 text-2xl font-bold text-nowrap flex justify-center"><TextTransition children={role} springConfig={presets.molasses} /></div>
             <div className="mt-16 flex justify-center gap-12 flex-col lg:flex-row items-center">
-                <div className="flex gap-2 text-lg" onClick={() => window.open("https://linkedin.com/in/nannan-", "_blank")}>
+                <div className="flex gap-2 text-lg cursor-pointer" onClick={() => window.open("https://linkedin.com/in/nannan-", "_blank")}>
                     <FaLinkedin className="w-8 h-8 fill-[#f694fb]" />
                     nannan-
                 </div>
-                <div className="flex gap-2 text-lg" onClick={() => window.open("https://github.com/nanna7077", "_blank")}>
+                <div className="flex gap-2 text-lg cursor-pointer" onClick={() => window.open("https://github.com/nanna7077", "_blank")}>
                     <FaGithubSquare className="w-8 h-8 fill-[#f694fb]" />
                     nanna7077
                 </div>
-                <div className="flex gap-2 text-lg" onClick={() => window.open("tel:+919445168005", "_blank")}>
+                <div className="flex gap-2 text-lg cursor-pointer" onClick={() => window.open("tel:+919445168005", "_blank")}>
                     <FaPhoneSquare className="w-8 h-8 fill-[#f694fb]" />
                     +919445168005
                 </div>
-                <div className="flex gap-2 text-lg" onClick={() => window.open("mailto:nanna7077@gmail.com", "_blank")}>
+                <div className="flex gap-2 text-lg cursor-pointer" onClick={() => window.open("mailto:nanna7077@gmail.com", "_blank")}>
                     <FaMailBulk className="w-8 h-8 fill-[#f694fb]" />
                     nanna7077@gmail.com
                 </div>
             </div>
-            {/* <div className="text-xl font-semibold text-nowrap mt-20">
-                Latest Blogs / Projects
-            </div>
-            <div className="mt-6 flex gap-4 flex-wrap justify-around">
-                <div className="p-4 bg-[rgba(246,148,251,.2)] backdrop-blur-md rounded-xl w-1/4 flex gap-2 flex-col max-h-[200px]">
-                    <div className="font-semibold text-lg">Blog 1</div>
-                    Blog desc
-                    <div className="self-end">
-                        21st April 2024
-                    </div>
-                </div>
-            </div> */}
         </div>
     );
 }
